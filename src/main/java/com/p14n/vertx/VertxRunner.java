a1bd8d176e85e96cb6a4d7735ced37e49e2ffedb
Dean Pehrsson-Chapman <dean@p14n.com>
2014-04-11 08:43:53 +0100

    Added cluster configuration

--- src/main/java/com/p14n/vertx/VertxRunner.java
+++ src/main/java/com/p14n/vertx/VertxRunner.java
@@ -16,6 +16,12 @@ import java.util.concurrent.CountDownLatch;
 import org.apache.log4j.Level;
 import com.hazelcast.config.*;
 import org.vertx.java.spi.cluster.impl.hazelcast.ProgrammableClusterManagerFactory;
+import java.util.Map;
+import java.util.HashMap;
+import java.util.List;
+import java.util.ArrayList;
+import java.io.FileReader;
+import java.io.BufferedReader;
 
 /**
  * Created by Dean Pehrsson-Chapman
@@ -23,55 +29,122 @@ import org.vertx.java.spi.cluster.impl.hazelcast.ProgrammableClusterManagerFacto
  */
 public class VertxRunner {
 
-  public static void cluster(int port,String... ips){
+  public static PlatformManager cluster(Map<String,String> settings) {
 
-    Config cfg = new Config();
-    ProgrammableClusterManagerFactory.setConfig(cfg);
-    System.setProperty("vertx.clusterManagerFactory", ProgrammableClusterManagerFactory.class.getName());
+    String cluster = settings.get("cluster");
+    if(cluster!=null){
+      System.out.println("Cluster values "+cluster);
+      String[] clusterVals = cluster.split(",");
 
-    NetworkConfig network = cfg.getNetworkConfig();
-    network.setPort(port);
-    network.setPortAutoIncrement(false);
-    Join join = network.getJoin();
-    join.getMulticastConfig().setEnabled(false);
-    for(String ip:ips){
-      join.getTcpIpConfig().addMember(ip);
+      int port = Integer.parseInt(clusterVals[0]);
+      String host = clusterVals[1];
+
+      Config cfg = new Config();
+      ProgrammableClusterManagerFactory.setConfig(cfg);
+      System.setProperty("vertx.clusterManagerFactory", ProgrammableClusterManagerFactory.class.getName());
+      NetworkConfig network = cfg.getNetworkConfig();
+      network.setPort(port);
+      network.setPortAutoIncrement(false);
+      Join join = network.getJoin();
+
+      String members = settings.get("members");
+      if(members!=null){
+        for(String ip:members.split(",")){
+          join.getTcpIpConfig().addMember(ip);
+        }
+      }
+      join.getMulticastConfig().setEnabled(members==null);
+      join.getTcpIpConfig().setEnabled(members!=null);
+
+      if(clusterVals.length == 2)
+        return PlatformLocator.factory.createPlatformManager(port, host);
+      if(clusterVals.length == 4)
+        return PlatformLocator.factory.createPlatformManager(port, host,Integer.parseInt(clusterVals[2]),clusterVals[3]);
+
     }
-    join.getTcpIpConfig().setEnabled(true);
-
+    return PlatformLocator.factory.createPlatformManager();
   }
     
-  private static void setupLogging() {
-    LoggingSetup.setupLogging("colqo.log","arch/colqo.log%d",Level.DEBUG);
+  private static void setupLogging(Map<String,String> settings) {
+    String[] logstuff = new String[]{"vertrunner.log","arch/vertrunner.log%d","DEBUG"};
+    if(settings.containsKey("log")){
+      System.out.println("Log values "+settings.get("log"));
+      logstuff = settings.get("log").split(",");
+    }
+    LoggingSetup.setupLogging(logstuff[0],logstuff[1],Level.toLevel(logstuff[2]));
     System.setProperty(
             "org.vertx.logger-delegate-factory-class-name",
             "org.vertx.java.core.logging.impl.SLF4JLogDelegateFactory"
     );
   }
-    
-  public static void main(String args[]) throws BrokenBarrierException, InterruptedException {
 
-    setupLogging();
+  public static List<String> findArgumentsFromArgumentFiles(String args[]){
+    List<String> params = new ArrayList<String>();
+    for(String arg:args){
+      System.out.println("Examine "+arg);
+      File f = new File(arg);
+      if(f.exists() && !f.isDirectory()){
+        System.out.println("Reading for args "+arg);
+        try {
+          BufferedReader b = new BufferedReader(new FileReader(f));
+          for(String line = "";line!=null;line=b.readLine()){
+            System.out.println("Found arg "+line);
+            params.add(line);
+          }
+          b.close();
+        } catch (Exception e){
+          e.printStackTrace();
+        }
+      } else {
+        params.add(arg);
+      }
+    }
+    return params;
+  }
+    
+  public static void main(String params[]) throws BrokenBarrierException, InterruptedException {
+
+    List<String> args = findArgumentsFromArgumentFiles(params);
+
+    Map<String,String> settings = filterSettings(args);
+    setupLogging(settings);
 
     //PlatformManager pm = PlatformLocator.factory.createPlatformManager();
-    cluster(5900);
+    PlatformManager pm = cluster(settings);
 
-    PlatformManager pm = PlatformLocator.factory.createPlatformManager(5900, "::1");
+
     deployAll(pm,args);
     //pm.undeployAll(null);
     Thread.currentThread().join();
     
+
+
   }
 
-  public static PlatformManager deployAll(PlatformManager pm, String... names) {
-    CountDownLatch latch = new CountDownLatch(names.length);
+  public static Map<String,String> filterSettings(List<String> params){
+    Map<String,String> settings = new HashMap<String,String>();
+    for(int i=0;i<params.size();i++){
+      String param = params.get(i);
+      int index = param.indexOf('=');
+      if(index>-1){
+        System.out.println("Found setting "+param);
+        settings.put(param.substring(0,index),param.substring(index+1,param.length()));
+        params.remove(i--);
+      }
+    }
+    for(Map.Entry<String,String> ent:settings.entrySet()){
+      System.out.println(ent.getKey()+" = "+ent.getValue());
+    }
+    return settings;
+  }
+
+  public static PlatformManager deployAll(PlatformManager pm, List<String> names) {
+    CountDownLatch latch = new CountDownLatch(names.size());
     int index = 1;
     for (String name : names) {
       int colon = name.indexOf(":");
       String cp = null;
       if(colon>0){
-
-        
         cp = name.substring(0,colon);
         name = name.substring(colon+1);
       }
